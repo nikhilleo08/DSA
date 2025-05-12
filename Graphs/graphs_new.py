57482b898c8dd1ecca917faf300cd3f80d48b000
Nikhil Ramrakhyani <nikhilramrakhyani8@gmail.com>
2025-05-12 19:33:03 +0000

    Added directed graph support

--- Graphs/graphs_new.py
+++ Graphs/graphs_new.py
@@ -1,6 +1,7 @@
 class Graph:
-    def __init__(self):
+    def __init__(self, directed):
         self.graph = {}
+        self.directed = directed
     
     def add_vertex(self, vertex):
         if vertex not in self.graph:
@@ -14,13 +15,13 @@ class Graph:
         
         if v2 not in self.graph[v1]:
             self.graph[v1].append(v2)
-        if v1 not in self.graph[v2]:
+        if not self.directed and v1 not in self.graph[v2]:
             self.graph[v2].append(v1)
     
     def remove_edge(self, v1, v2):
         if v1 in self.graph and v2 in self.graph[v1]:
             self.graph[v1].remove(v2)
-        if v2 in self.graph and v1 in self.graph[v2]:
+        if not self.directerd and v2 in self.graph and v1 in self.graph[v2]:
             self.graph[v2].remove(v1)
 
     def remove_vertex(self, vertex):
@@ -29,32 +30,23 @@ class Graph:
                 self.graph[neighbour].remove(vertex)
             del self.graph[vertex]
 
+        # Remove incoming edges to this vertex (only needed for directed graphs)
+        for v in self.graph:
+            if vertex in self.graph[v]:
+                self.graph[v].remove(vertex)
+
     def print_graph(self):
         for vertex, neighbors in self.graph.items():
             print(f"{vertex}: {neighbors}")
 
 
-graph = Graph()
-graph.add_vertex('A')    
-graph.add_vertex('B')    
-graph.add_vertex('C')
-graph.print_graph()
-graph.add_edge('A', 'B')
-graph.add_edge('B', 'C')
-graph.add_edge('C', 'A')
-graph.print_graph()
-print('NEW GRAPH')
-g = Graph()
-g.add_vertex("A")
-g.add_vertex("B")
+g = Graph(directed=False)
 g.add_edge("A", "B")
-g.add_edge("A", "C")
-g.add_edge("B", "D")
-print('Before removing edge from "C" to "A"')
+g.add_edge("B", "C")
 g.print_graph()
-g.remove_edge('C','A')
-print('After removing edge from "C" to "A"')
+
+
+g = Graph(directed=True)
+g.add_edge("A", "B")
+g.add_edge("B", "C")
 g.print_graph()
-graph.print_graph()
-graph.remove_vertex('C')
-graph.print_graph()
